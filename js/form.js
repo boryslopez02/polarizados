d3a8f2c7fd599e9a4dd2a78197f234f33ea35cc4
Borys Lopez <68643829+boryslopez02@users.noreply.github.com>
2021-09-05 04:40:53 -0430

    form - modals - finish

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
 const form = document.getElementById('contact-form');
 let text = document.getElementById('ui-text'),
+textSend = document.getElementById('ui-text2'),
+textError = document.getElementById('ui-text3'),
+textErrorEmail = document.getElementById('ui-text4'),
 name = document.getElementById('namef'),
 email = document.getElementById('emailf'),
 msg = document.getElementById('mensajef'),
@@ -7,16 +10,23 @@ emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
 form.addEventListener('submit', e => {
     e.preventDefault();
+    text.style.display = 'none';
     if (emailRegex.test(email.value)) {
         if (name.value.trim() && email.value.trim() && msg.value.trim()) {
-            text.innerText = "Mensaje enviado satisfactoriamente, nos pondremos en contacto contigo a la brevedad !";
+            textSend.style.display = 'block';
             setInterval(() => {
                 form.submit();
             }, 3500);
         } else {
-            text.innerText = "Ingrese los datos correctamente, todos los campos son obligatorios !";
+            name.value='';
+            email.value='';
+            msg.value = '';
+            textError.style.display = 'block';
         }
     } else {
-        text.innerText = "Ingrese un Email válido !";
+        name.value='';
+        email.value='';
+        msg.value = '';
+        textErrorEmail.style.display = 'block';
     }
 });
